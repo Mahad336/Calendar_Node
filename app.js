ba28e764004ee9c53c499d6c57f5c82a443f3ea2
Mahad366 <mahad.younis@gigalabs.co>
2023-01-02 16:50:28 +0500

    Calendar_node

--- app.js
+++ app.js
@@ -39,4 +39,3 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(authRoutes);
 app.use(calendarRoutes);
-app.use(smoothieRoutes);
